b13a57d4e00db5876001b71fea37ce272251848d
priyanka mohole <priyankamohole@gmail.com>
2026-08-23 19:29:13 +0530

    fundamentals

---
+++ Fundamentals/infiniteloop.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class infiniteloop{
+
+    public static void main(String args[]){
+
+        do{
+            System.out.print("Hello World!");
+        }
+        while(true);
+
+    }
+}
